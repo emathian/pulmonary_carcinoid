923bfafd9e8414da87ce58a7f2b1c6f468406e9f
Emi <emilie.mathian@insa-lyon.fr>
2019-05-22 14:13:43 +0200

    CP by level k

--- pulmonary_carcinoids/pulmonary_carcinoids_CODE/comparaison_maps/Graph_Comparaison_Meso.R
+++ pulmonary_carcinoids/pulmonary_carcinoids_CODE/comparaison_maps/Graph_Comparaison_Meso.R
@@ -375,7 +375,7 @@ CP_PCA_TM <- read.table("CP_MesosomicsV2.txt", sep = "\t", dec="." , header = TR
 CP_R_PCA <- read.table("CP_meso_PCA_real.txt", sep = "\t", dec="." , header = TRUE,   quote="")
 CP_R_TM <- read.table("CP_meso_TM_real.txt", sep = "\t", dec="." , header = TRUE,   quote="")
 CP_R_UMAP_NN150_MD05 <- read.table("CP_meso_UMAP_NN150_MD05_real.txt", sep = "\t", dec="." , header = TRUE,   quote="")
-par(mfrow=c(2,2))
+
 CP_R_UMAP_NN230 <- read.table("CP_meso_UMAP_NN230_real.txt", sep = "\t", dec="." , header = TRUE,   quote="")
 CP_R_UMAP_NN20 <- read.table("CP_meso_UMAP_NN20_real.txt", sep = "\t", dec="." , header = TRUE,   quote="")
 CP_R_UMAP_MD09 <- read.table("CP_meso_UMAP_MD09_real.txt", sep = "\t", dec="." , header = TRUE,   quote="")
@@ -452,9 +452,52 @@ plot(CP_R_UMAP_MD02$CP2[which(CP_R_UMAP_MD02$K == 170)], CP_R_UMAP_MD02$CPN[whic
 plot(CP_R_UMAP_MD02$CP2[which(CP_R_UMAP_MD02$K == 280)], CP_R_UMAP_MD02$CPN[which(CP_R_UMAP_MD02$K == 280)], xlab="CP_UMAP_MD02",  ylab="CPN")
 par(mfrow=c(1,1))
 
+CP_mean_by_k  <-function (list_CP_diff , Name){
+  par(mfrow=c(1,2))
+  c= 1 
+  c_data_frame = as.data.frame(list_CP_diff[1])
+  CP_diff_mean_df = data.frame("k" =unique(c_data_frame$K ))
+  for (i in 1:length(list_CP_diff)){
+    c_data_frame = as.data.frame(list_CP_diff[i])
+    diff_CP2_CPN = abs(c_data_frame$CP2 - c_data_frame$CPN )
+    c_data_frame = cbind(c_data_frame ,diff_CP2_CPN )
+    colnames(c_data_frame)[dim(c_data_frame)[2]] <- "Abs_diff"
+    Mean_by_k =tapply(c_data_frame$Abs_diff ,c_data_frame$K, mean)
+    CP_diff_mean_df <- cbind(CP_diff_mean_df ,Mean_by_k )
+    colnames(CP_diff_mean_df)[dim(CP_diff_mean_df)[2]] <- Name[i]
+  
+  }
+  print(dim(CP_diff_mean_df))
+  Col = c()
+  C=1
+  for (i in 2:dim(CP_diff_mean_df)[2]){
+    
+    Col = c(Col,C)
+    if (i ==2 ){
+      plot(CP_diff_mean_df$k, CP_diff_mean_df[,i] , col=C  , type ='l' )
+      C =C+1
+      Col = c(Col,C)
+    }
+    else{
+      lines(CP_diff_mean_df$k, CP_diff_mean_df[,i] , col=C )
+      C =C+1
+      Col = c(Col,C)
+    }
+    
+  }
+  print(unique(Col))
+  plot.new()
+  legend("bottomleft", 
+         legend = Name, 
+         col = c(unique(Col)),
+         pch = 15)
+}
 
 
+list_CP_df = list(CP_PCA_TM , CP_R_PCA , CP_R_TM , CP_R_UMAP_NN150_MD05 , CP_R_UMAP_NN230 , CP_R_UMAP_NN20 , CP_R_UMAP_MD09, CP_R_UMAP_MD02)
+Name = c("CP_PCA_TM" , "CP_R_PCA" , "CP_R_TM", "CP_R_UMAP_NN150_MD05" , "CP_R_UMAP_NN230" , "CP_R_UMAP_NN20" , "CP_R_UMAP_MD09", "CP_R_UMAP_MD02")
 
+CP_mean_by_k(list_CP_df,Name)
 # Set Diff
 # ---------
 
@@ -483,23 +526,79 @@ library(ggplot2)
 #        Set_diff_R_TM$set_diff[Set_diff_R_TM$k==20], Set_diff_R_UMAP_NN150_MD_05$set_diff[Set_diff_R_UMAP_NN150_MD_05$k==20],
 #        Set_diff_R_UMAP_NN20$set_diff[Set_diff_R_UMAP_NN20$k==20])
 
-dist_set <- function(K , list_data_frame ){
-  name_list_data_frame <- as.character(list_data_frame)
+dist_set <- function(K , list_data_frame , Name ){
+  
   par(mfrow= c(length(list_data_frame),length(K))) 
   c = 1
    for (j in 1:length(list_data_frame )){
     for (i in 1:length(K)) {
         c_data_frame = as.data.frame(list_data_frame[j])
-        plot(c_data_frame$set_diff[c_data_frame$k==K[i]], col = c, main = paste("k = ", K[i] , as.character(list_data_frame[j])),  xlab="Index", ylab="set diff value")
+        plot(c_data_frame$set_diff[c_data_frame$k==K[i]], col = c, main = paste("k = ", K[i] , Name[j]),  xlab="Index", ylab="set diff value")
       }
     c =c+1
   }  
 }
 
-list_df_set= list(Set_diff_PCA_TM, Set_diff_R_PCA, Set_diff_R_TM, Set_diff_R_UMAP_NN150_MD_05)
-#, Set_diff_R_UMAP_NN230, Set_diff_R_UMAP_NN20,Set_diff_R_UMAP_MD09,Set_diff_R_UMAP_MD02
+#list_df_set= list(Set_diff_PCA_TM, Set_diff_R_PCA, Set_diff_R_TM, Set_diff_R_UMAP_NN150_MD_05)
+list_df_set= list(Set_diff_R_UMAP_NN230, Set_diff_R_UMAP_NN20,Set_diff_R_UMAP_MD09,Set_diff_R_UMAP_MD02)
+#Name_comp = c("PCA_TM", "R_PCA", "R_TM", "R_UMAP_NN150_MD_05")
+Name_comp = c("R_UMAP_NN230", "R_UMAP_NN20","R_UMAP_MD09","R_UMAP_MD02")
+dist_set(c(20,40,60,100),list_df_set, Name_comp )
 
-dist_set(c(20,40,60,100),list_df_set)
+plot(x=unique(Set_diff_PCA_TM$k) ,y= tapply(Set_diff_PCA_TM$set_diff ,Set_diff_PCA_TM$k, mean))
+
+Set_diff_mean_by_k  <-function (list_set_diff , Name){
+  par(mfrow=c(1,2))
+  c= 1 
+  c_data_frame = as.data.frame(list_set_diff[1])
+  Set_diff_mean_df = data.frame("k" =unique(c_data_frame$k ))
+  for (i in 1:length(list_set_diff)){
+    c_data_frame = as.data.frame(list_set_diff[i])
+    Mean_by_k =tapply(c_data_frame$set_diff ,c_data_frame$k, mean)
+    Set_diff_mean_df <- cbind(Set_diff_mean_df,Mean_by_k )
+    colnames(Set_diff_mean_df)[dim(Set_diff_mean_df)[2]] <- Name[i]
+   # print(head(Set_diff_mean_df))
+    
+  }
+  Col = c()
+  C=1
+  for (i in 2:dim(Set_diff_mean_df)[2]){
+   
+    Col = c(Col,C)
+    if (i ==2 ){
+        plot(Set_diff_mean_df$k, Set_diff_mean_df[,i] , col=C  , type ='l' )
+      C =C+1
+      Col = c(Col,C)
+    }
+    else{
+      lines(Set_diff_mean_df$k, Set_diff_mean_df[,i] , col=C )
+     
+     
+      C =C+1
+      Col = c(Col,C)
+    }
+  
+  }
+  print(unique(Col))
+  plot.new()
+  legend("bottomleft", 
+         legend = Name, 
+         col = c(unique(Col)),
+         pch = 15)
+
+
+}
+    
+    
+ 
+
+list_df_set= list(Set_diff_R_PCA, Set_diff_R_TM, Set_diff_R_UMAP_NN150_MD_05 ,Set_diff_PCA_TM, 
+                  Set_diff_R_UMAP_NN230, Set_diff_R_UMAP_NN20,Set_diff_R_UMAP_MD09, Set_diff_R_UMAP_MD02)
+Name = c( "R_PCA", "R_TM", "R_UMAP_NN150_MD_05","PCA_TM",  "R_UMAP_NN230", "R_UMAP_NN20","R_UMAP_MD09","R_UMAP_MD02")
+testP <- Set_diff_mean_by_k(list_df_set, Name)
+testP 
+
+# CONCLUSION :  TRES PEU DE DIFFERENCE ENTRE LES MOYENNES
 
 # Seq Diff 
 # --------
@@ -529,5 +628,49 @@ plot(Seq_diff_R_TM$seq_diff)
 
 
 
+Seq_diff_mean_by_k  <-function (list_Seq_diff , Name){
+  par(mfrow=c(1,2))
+  c= 1 
+  c_data_frame = as.data.frame(list_Seq_diff[1])
+  Seq_diff_mean_df = data.frame("k" =unique(c_data_frame$k ))
+  for (i in 1:length(list_Seq_diff)){
+    c_data_frame = as.data.frame(list_Seq_diff[i])
+    Mean_by_k =tapply(c_data_frame$seq_diff ,c_data_frame$k, mean)
+    Seq_diff_mean_df <- cbind(Seq_diff_mean_df,Mean_by_k )
+    colnames(Seq_diff_mean_df)[dim(Seq_diff_mean_df)[2]] <- Name[i]
+    # print(head(Seq_diff_mean_df))
+    
+  }
+  Col = c()
+  C=1
+  for (i in 2:dim(Seq_diff_mean_df)[2]){
+    
+    Col = c(Col,C)
+    if (i ==2 ){
+      plot(Seq_diff_mean_df$k, Seq_diff_mean_df[,i] , col=C  , type ='l' , )
+      C =C+1
+      Col = c(Col,C)
+    }
+    else{
+      lines(Seq_diff_mean_df$k, Seq_diff_mean_df[,i] , col=C )
+      C =C+1
+      Col = c(Col,C)
+    }
+    
+  }
+  print(unique(Col))
+  plot.new()
+  legend("bottomleft", 
+         legend = Name, 
+         col = c(unique(Col)),
+         pch = 15)
+}
 
+list_df_seq= list( Seq_diff_PCA_TM ,Seq_diff_R_PCA ,Seq_diff_R_TM , Seq_diff_R_UMAP_NN230 ,
+                   Seq_diff_R_UMAP_NN20, Seq_diff_R_UMAP_MD09 ,Seq_diff_R_UMAP_MD02,Seq_diff_R_UMAP_MD05_NN150 )
 
+Name= c( 'R_PCA' , 'R_TM' , 'R_UMAP_NN230' ,
+          'R_UMAP_NN20', 'R_UMAP_MD09' , 'R_UMAP_MD02','R_UMAP_MD05_NN150')
+#'PCA_TM' , 
+Seq_diff_mean_by_k(list_df_seq,Name)
+        
